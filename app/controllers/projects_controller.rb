ca90aef430458c1b6030826c84391a924888a7a6
lemonjai <leocheung_1986@hotmail.com>
2017-05-24 21:56:36 -0400

    updated the controller for work and project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,51 @@
 class ProjectsController < ApplicationController
+
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project, notice: "Project has been saved!!!"
+    else
+      render 'edit'
+    end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new blog_params
+
+    if @project.save
+      redirect_to @project, notice: "Project has been saved!!!"
+    else
+      render 'new', notice: "Unable to save!!!"
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:title, :content, :img)
+    end
+
+    def find_post
+      @project = Project.find(params[:id])
+    end
+
+
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,50 @@
 class WorksController < ApplicationController
+
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @work.update work_params
+      redirect_to @work, notice: "Work has been saved!!!"
+    else
+      render 'edit'
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new work_params
+
+    if @work.save
+      redirect_to @work, notice: "Work has been saved!!!"
+    else
+      render 'new', notice: "Unable to save!!!"
+    end
+  end
+
+  def destroy
+    @work.destroy
+    redirect_to works_path
+  end
+
+  private
+    def work_params
+      params.require(:work).permit(:title, :content)
+    end
+
+    def find_post
+      @work = Work.find(params[:id])
+    end
+
 end
