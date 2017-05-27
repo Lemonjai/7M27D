eefff5ae5cc92d33bc0095e1706b13192e0570ef
lemonjai <leocheung_1986@hotmail.com>
2017-05-26 22:27:38 -0400

    updated the work and project to update dynamically

--- app/controllers/spa_controller.rb
+++ app/controllers/spa_controller.rb
@@ -1,5 +1,7 @@
 class SpaController < ApplicationController
   def main
     @blogs = Blog.all.order("created_at desc")
+    @works = Work.all.order("created_at desc")
+    @projects = Project.all.order("created_at desc")
   end
 end
